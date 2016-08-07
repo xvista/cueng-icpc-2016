457053053f91a81736077877ba28efaeeff51388
Tananan Tangthanachaikul <microbenz.prob@gmail.com>
2016-08-07 19:53:12 +0700

    Central A: Add preparation course page

--- resources/views/2016/partials/preparation-course.blade.php
+++ resources/views/2016/partials/preparation-course.blade.php
@@ -1,1 +1,57 @@
-<h3>Preparation Course</h3>
+<h3>การอบรม</h3>
+<p>
+    ทางคณะวิศวกรรมศาสตร์ จุฬาลงกรณ์มหาวิทยาลัย ได้เปิดการอบรมเพื่อเตรียมพร้อมสำหรับการแข่งขัน ACM-ICPC ดังตารางต่อไปนี้
+</p>
+<table class="table table-bordered">
+    <tr class="table-header">
+        <th>วัน</th>
+        <th>เวลา</th>
+        <th>หัวข้อการอบรม</th>
+        <th>วิทยากร</th>
+        <th>เอกสารประกอบการอบรม</th>        
+    </tr>
+    <tr>
+        <td>วันเสาร์ 27 สิงหาคม 2559</td>
+        <td>8.00 น. - 17.00 น.</td>
+        <td></td>
+        <td>ผศ.ดร.นัทที นิภานันท์</td>
+        <td>
+            <a>
+                <span class="glyphicon glyphicon-download-alt"></span> Download
+            </a>
+        </td>
+    </tr>
+    <tr>
+        <td>วันอาทิตย์ 28 สิงหาคม 2559</td>
+        <td>8.00 น. - 17.00 น.</td>
+        <td></td>
+        <td>ผศ.ดร.สุกรี สินธุภิญโญ</td>
+        <td>
+            <a>
+                <span class="glyphicon glyphicon-download-alt"></span> Download
+            </a>
+        </td>
+    </tr>
+    <tr>
+        <td>วันเสาร์ 3 กันยายน 2559</td>
+        <td>8.00 น. - 17.00 น.</td>
+        <td></td>
+        <td>อาจารย์ ดร.พิชญะ สิทธีอมร</td>
+        <td>
+            <a>
+                <span class="glyphicon glyphicon-download-alt"></span> Download
+            </a>
+        </td>
+    </tr>
+    <tr>
+        <td>วันอาทิตย์ 4 กันยายน 2559</td>
+        <td>8.00 น. - 17.00 น.</td>
+        <td></td>
+        <td>อาจารย์ ดร.ณัฐพงศ์ ชินธเนศ</td>
+        <td>
+            <a>
+                <span class="glyphicon glyphicon-download-alt"></span> Download
+            </a>
+        </td>
+    </tr>
+</table>
