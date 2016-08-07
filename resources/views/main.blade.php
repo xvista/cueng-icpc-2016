2b5b94a52953b8267812294f17f68f4d22e1cbfc
Tananan Tangthanachaikul <microbenz.prob@gmail.com>
2016-08-07 08:36:45 +0700

    Central A : Change Welcoming section using class instead id for styling

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -6,10 +6,10 @@
 
 @section('content')
 <header>
-    <div id="central-a-contest-section">
-        <img src="{{ asset('img/icpc_logo.png') }}" id="contest-logo">
-        <h1 id="contest-title">2016 ACM-ICPC Thailand Central A Contest</h1>
-        <h3 id="contest-description">Veniam nulla quis amet anim consequat magna proident nisi esse.</h3>
+    <div id="central-a-contest-section" class="welcoming-section">
+        <img src="{{ asset('img/icpc_logo.png') }}" class="contest-logo">
+        <h1 class="contest-title">2016 ACM-ICPC Thailand Central A Contest</h1>
+        <h3 class="contest-description">Veniam nulla quis amet anim consequat magna proident nisi esse.</h3>
         <a href="{{ url('/2016/thailand/central-a') }}" class="btn btn-primary ghost-white-button" id="central-a-button">More Info</a>
     </div>
 </header>
