0c72f981472248bd00bc27487234db36434689dd
Tananan Tangthanachaikul <microbenz.prob@gmail.com>
2016-08-10 12:04:29 +0700

    Central A: Add Contestant Environment Page

--- resources/views/2016/partials/contestant.blade.php
+++ resources/views/2016/partials/contestant.blade.php
@@ -1,1 +1,35 @@
-<h3>Contestant</h3>
+<h3>Environment</h3>
+<ul>
+    <li><b>Processor:</b> Intel(R) Core(TM) i5</li>
+    <li><b>RAM:</b> 4GB</li>
+    <li><b>HDD:</b> 30GB</li>
+    <li><b>Monitor:</b> 21-inch widescreen LCD monitor</li>
+    <li><b>Operating System:</b> Ubuntu 12.04 32-bit</li>
+    <li><b>Grader:</b> PC2 v9.2.3</li>
+    <li><b>Compiler command line:</b></li>
+    <li>
+        <ul>
+            <li>GNU C gcc -g -O2 -std=gnu99 -static $* -lm</li>
+            <li>GNU C++ g++ -g -O2 -std=gnu++0x -static $*</li>
+            <li>Java javac -encoding UTF-8 -sourcepath . -d . $*</li>
+        </ul>
+    </li>
+    <li><b>Compiler Version:</b></li>
+    <ul>
+        <li>gcc version 4.8.2 (Ubuntu 4.8.2-19ubuntu1)</li>
+        <li>g++ version 4.8.2 (Ubuntu 4.8.2-19ubuntu1)</li>
+        <li>java version "1.7.0_65" <br>
+        OpenJDK Runtime Environment (IcedTea 2.5.2) (7u65-2.5.2-3~14.04) <br>
+        OpenJDK 64-Bit Server VM (build 24.65-b04, mixed mode)</li>
+    </ul>
+    <li><b>Editor:</b></li>
+    <ul>
+        <li>vi/vim</li>
+        <li>gvim</li>
+        <li>gedit</li>
+        <li>emacs</li>
+        <li>eclipse</li>
+        <li>codeblocks</li>
+        <li>geany</li>
+    </ul>
+</ul>
