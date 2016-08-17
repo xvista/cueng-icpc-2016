b179247ac1222c6b544ead6fa71cd431b1359989
Atikhun Orisoon <genatorster@gmail.com>
2016-08-17 20:22:05 +0700

    Central A: Contestant info

--- resources/views/2016/partials/contestant.blade.php
+++ resources/views/2016/partials/contestant.blade.php
@@ -4,23 +4,24 @@
     <li><b>RAM:</b> 4GB</li>
     <li><b>HDD:</b> 30GB</li>
     <li><b>Monitor:</b> 21-inch widescreen LCD monitor</li>
-    <li><b>Operating System:</b> Ubuntu 12.04 32-bit</li>
-    <li><b>Grader:</b> PC2 v9.2.3</li>
+    <li><b>Operating System:</b> Ubuntu 16.04 64-bit</li>
+    <li><b>Grader:</b> PC2 v9.3.2</li>
     <li><b>Compiler command line:</b></li>
     <li>
         <ul>
             <li>GNU C <code>gcc -g -O2 -std=gnu99 -static $* -lm</code></li>
-            <li>GNU C++ <code>g++ -g -O2 -std=gnu++0x -static $*</code></li>
+            <li>GNU C++ <code>g++ -g -O2 -std=gnu++11 -static $*</code></li>
             <li>Java <code>javac -encoding UTF-8 -sourcepath . -d . $*</code></li>
         </ul>
     </li>
     <li><b>Compiler Version:</b></li>
     <ul>
-        <li>gcc version 4.8.2 (Ubuntu 4.8.2-19ubuntu1)</li>
-        <li>g++ version 4.8.2 (Ubuntu 4.8.2-19ubuntu1)</li>
-        <li>java version "1.7.0_65" <br>
-        OpenJDK Runtime Environment (IcedTea 2.5.2) (7u65-2.5.2-3~14.04) <br>
-        OpenJDK 64-Bit Server VM (build 24.65-b04, mixed mode)</li>
+        <li>gcc version 5.4.0 (Ubuntu 5.4.0-6ubuntu1~16.04.1)</li>
+        <li>g++ version 5.4.0 (Ubuntu 5.4.0-6ubuntu1~16.04.1)</li>
+        <li>java version "1.8.0_91" <br>
+        OpenJDK Runtime Environment (build 1.8.0_91-8u91-b14-3ubuntu1~16.04.1-b14)<br>
+        OpenJDK 64-Bit Server VM (build 25.91-b14, mixed mode)
+        </li>
     </ul>
     <li><b>Editor:</b></li>
     <ul>
