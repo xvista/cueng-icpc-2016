5a84800978b8e58a8a0c9ca0710c8efce62d679c
Atikhun Orisoon <genatorster@gmail.com>
2016-09-15 02:55:13 +0700

    Central A: Results

--- resources/views/2016/partials/results.blade.php
+++ resources/views/2016/partials/results.blade.php
@@ -1,1 +1,107 @@
 <h3>Results</h3>
+<table class="table table-striped">
+    <thead>
+        <tr>
+            <th>อันดับที่</th>
+            <th>รางวัล</th>
+            <th>ทีม</th>
+            <th>สถาบัน</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1</td>
+            <td>ชนะเลิศ</td>
+            <td><b>Debuggers</b></td>
+            <td>จุฬาลงกรณ์มหาวิทยาลัย</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>รองชนะเลิศอันดับ 1</td>
+            <td><b>Wai Roon Jud Pai</b></td>
+            <td>จุฬาลงกรณ์มหาวิทยาลัย</td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>รองชนะเลิศอันดับ 1</td>
+            <td><b>MoonOTea</b></td>
+            <td>จุฬาลงกรณ์มหาวิทยาลัย</td>
+        </tr>
+        <tr>
+            <td>4</td>
+            <td>ชมเชย</td>
+            <td><b>footLongHamCheese</b></td>
+            <td>จุฬาลงกรณ์มหาวิทยาลัย</td>
+        </tr>
+        <tr>
+            <td>5</td>
+            <td>ชมเชย</td>
+            <td><b>EZDC</b></td>
+            <td>จุฬาลงกรณ์มหาวิทยาลัย</td>
+        </tr>
+        <tr>
+            <td>6</td>
+            <td></td>
+            <td><b>EZAC</b></td>
+            <td>จุฬาลงกรณ์มหาวิทยาลัย</td>
+        </tr>
+        <tr>
+            <td>7</td>
+            <td></td>
+            <td><b>IdeaPocket Returns</b></td>
+            <td>จุฬาลงกรณ์มหาวิทยาลัย</td>
+        </tr>
+        <tr>
+            <td>8</td>
+            <td></td>
+            <td><b>Fmmmmmm</b></td>
+            <td>จุฬาลงกรณ์มหาวิทยาลัย</td>
+        </tr>
+        <tr>
+            <td>9</td>
+            <td></td>
+            <td><b>Untitled-1</b></td>
+            <td>จุฬาลงกรณ์มหาวิทยาลัย</td>
+        </tr>
+        <tr>
+            <td>10</td>
+            <td></td>
+            <td><b>TeamChuanChom</b></td>
+            <td>จุฬาลงกรณ์มหาวิทยาลัย</td>
+        </tr>
+    </tbody>
+</table>
+<h4>รางวัลพิเศษ</h4>
+<table class="table table-striped">
+    <thead>
+        <tr>
+            <th>รางวัล</th>
+            <th>ทีม</th>
+            <th>สถาบัน</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>ความพยายามสูงส่ง</td>
+            <td><b>SE-Whatever</b></td>
+            <td>สถาบันเทคโนโลยีพระจอมเกล้าเจ้าคุณทหารลาดกระบัง</td>
+        </tr>
+        <tr>
+            <td>1 Shot 1 Kill</td>
+            <td><b>KMUTNB_ULIFE</b></td>
+            <td>มหาวิทยาลัยเทคโนโลยีพระจอมเกล้าพระนครเหนือ</td>
+        </tr>
+        <tr>
+            <td>Last Shot</td>
+            <td><b>MightyTiti</b></td>
+            <td>จุฬาลงกรณมหาวิทยาลัย</td>
+        </tr>
+        <tr>
+            <td>Today</td>
+            <td><b>CapsLock</b></td>
+            <td>มหาวิทยาลัยเกษตรศาสตร์ วิทยาเขตศรีราชา</td>
+        </tr>
+    </tbody>
+</table>
+<h4>Scoreboard</h4>
+<a class="btn btn-lg btn-primary" href="{{ url('2016/thailand/central-a/scoreboard') }}" target="_blank">ดู Scoreboard</a>
