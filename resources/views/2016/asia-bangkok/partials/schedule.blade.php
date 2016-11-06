43d3069e7acb8153aa9c5b27664d79dbd8c43b63
Atikhun Orisoon <genatorster@gmail.com>
2016-11-07 02:39:11 +0700

    Asia Bangkok: full schedule

--- resources/views/2016/asia-bangkok/partials/schedule.blade.php
+++ resources/views/2016/asia-bangkok/partials/schedule.blade.php
@@ -1,48 +1,74 @@
 <h3>Tentative Schedule</h3>
+<ul>
+    <li>
+        <b>Registration</b><br>
+        Ground Floor Engineering Centennial Memorial Building, Faculty of Engineering, Chulalongkorn University
+    </li>
+    <li>
+        <b>Opening Ceremony</b> <br>
+        Room 117 Engineering Building 3, Faculty of Engineering, Chulalongkorn University
+    </li>
+    <li>
+        <b>Practice & Contest Session</b> <br>
+        Floor 12 Engineering Centennial Memorial Building, Faculty of Engineering, Chulalongkorn University
+    </li>
+    <li>
+        <b>Coach Room</b> <br>
+        Floor 11 Engineering Centennial Memorial Building, Faculty of Engineering, Chulalongkorn University
+    </li>
+    <li>
+        <b>Awards Ceremony & Dinner</b> <br>
+        Petchpailin Room, Windsor Suites Hotel, 10/1 Sukhumvit Soi 20, Sukhumvit Road, Bangkok
+    </li>
+    <li>
+        <b>Lunch</b> <br>
+        Floor 2 Engineering Building 4, Faculty of Engineering, Chulalongkorn University
+    </li>
+</ul>
 <table class="table table-bordered">
     <thead>
         <tr>
-            <th colspan="3">Friday, 11 November: Registration, Opening and Practice Session at Faculty of Engineering, Chulalongkorn University</th>
+            <th colspan="3">Friday, 11 November:<br>Registration, Opening and Practice Session at Faculty of Engineering, Chulalongkorn University</th>
         </tr>
         <tr>
-            {{-- <th style="width: 15%; text-align: center">Start - End</th> --}}
+            <th style="width: 15%; text-align: center"></th>
             <th style="width: 70%; text-align: center">Description</th>
             <th style="width: 30%; text-align: center">Note</th>
         </tr>
     </thead>
     <tbody>
         <tr>
-            {{-- <td style="text-align: center">from 07:00</td> --}}
+            <td style="text-align: center">07:00 - 08:30</td>
             <td>Pick up from Hotels to Faculty of Engineering CU</td>
             <td style="text-align: center">All</td>
         </tr>
         <tr>
-            {{-- <td style="text-align: center">08:30 - 09:30</td> --}}
+            <td style="text-align: center">08:00 - 09:00</td>
             <td>Registration, T-shirts</td>
             <td style="text-align: center"></td>
         </tr>
         <tr>
-            {{-- <td style="text-align: center">09:30 - 11:00</td> --}}
+            <td style="text-align: center">09:00 - 11:00</td>
             <td>Opening Ceremony</td>
             <td style="text-align: center">All</td>
         </tr>
         <tr>
-            {{-- <td style="text-align: center">11:15 - 11:30</td> --}}
+            <td style="text-align: center">11:00 - 11:30</td>
             <td>Photo Session</td>
             <td style="text-align: center">All</td>
         </tr>
         <tr>
-            {{-- <td style="text-align: center">11:30 - 13:00</td> --}}
+            <td style="text-align: center">11:30 - 12:30</td>
             <td>Lunch</td>
             <td style="text-align: center">All</td>
         </tr>
         <tr>
-            {{-- <td style="text-align: center">13:00 - 15:00</td> --}}
+            <td style="text-align: center">12:30 - 14:30</td>
             <td>Practice Session</td>
             <td style="text-align: center">Contestants</td>
         </tr>
         <tr>
-            {{-- <td style="text-align: center">15:15 - 15:30</td> --}}
+            <td style="text-align: center">15:00</td>
             <td>Bus transportation from Faculty of Engineering CU to hotels</td>
             <td style="text-align: center">All</td>
         </tr>
@@ -52,43 +78,51 @@
 <table class="table table-bordered">
     <thead>
         <tr>
-            <th colspan="3">Saturday, 12 November: Contest, Awards Ceremony ACM-ICPC Asia Bangkok 2016 at Faculty of Engineering, Chulalongkorn University</th>
+            <th colspan="3">Saturday, 12 November:<br>
+            Contest Session at Faculty of Engineering, Chulalongkorn University <br>
+            Awards Ceremony & Dinner at Petchpailin Room, Windsor Suites Hotel <br>
+            10/1 Sukhumvit Soi 20, Sukhumvit Road, Bangkok</th>
         </tr>
         <tr>
-            {{-- <th style="width: 15%; text-align: center">Start - End</th> --}}
+            <th style="width: 15%; text-align: center"></th>
             <th style="width: 70%; text-align: center">Description</th>
             <th style="width: 30%; text-align: center">Note</th>
         </tr>
     </thead>
     <tbody>
         <tr>
-            {{-- <td style="text-align: center">from 06:40</td> --}}
+            <td style="text-align: center">08:00 - 09:00</td>
             <td>Pick up from Hotel to Faculty of Engineering, Chulalongkorn University</td>
             <td style="text-align: center">All</td>
         </tr>
         <tr>
-            {{-- <td style="text-align: center">07:30 - 08:15</td> --}}
+            <td style="text-align: center">09:30 - 10:30</td>
+            <td>Registration</td>
+            <td style="text-align: center">Contestants</td>
+        </tr>
+        <tr>
+            <td style="text-align: center">10:30 - 11:00</td>
             <td>Contestants prepare to entrance to contest room</td>
             <td style="text-align: center">Contestants</td>
         </tr>
         <tr>
-            {{-- <td style="text-align: center">08:30 - 13:30</td> --}}
+            <td style="text-align: center">11:00 - 16:00</td>
             <td>The Competition</td>
             <td style="text-align: center">Contestants</td>
         </tr>
         <tr>
-            {{-- <td style="text-align: center">13:30 - 14:30</td> --}}
-            <td>Lunch & Beverage</td>
+            <td style="text-align: center">16:30 - 17:30</td>
+            <td>Bus transportation to Windsor Suites Hotel hotels</td>
             <td style="text-align: center">All</td>
         </tr>
         <tr>
-            {{-- <td style="text-align: center">14:40 - 18:50</td> --}}
-            <td>Leisure time or Bangkok tour</td>
+            <td style="text-align: center">17:30 - 21:00</td>
+            <td>Dinner, Celebration, results announcement and awards</td>
             <td style="text-align: center">All</td>
         </tr>
         <tr>
-            {{-- <td style="text-align: center">18:50 - 21:30</td> --}}
-            <td>Dinner, Celebration, results announcement and awards.</td>
+            <td style="text-align: center">21:30</td>
+            <td>Bus transportation from Windsor Suites Hotel to hotels</td>
             <td style="text-align: center">All</td>
         </tr>
     </tbody>
@@ -97,8 +131,26 @@
 <table class="table table-bordered">
     <thead>
         <tr>
-            <th colspan="3">Sunday, 13 November: Excursion</th>
+            <th colspan="3">Sunday, 13 November:<br>
+            Excursion at Ayutthaya</th>
+        </tr>
+        <tr>
+            <th style="width: 15%; text-align: center"></th>
+            <th style="width: 70%; text-align: center">Description</th>
+            <th style="width: 30%; text-align: center">Note</th>
         </tr>
     </thead>
+    <tbody>
+        <tr>
+            <td style="text-align: center">07:15</td>
+            <td>Departure from Bangkok to Ayutthaya</td>
+            <td style="text-align: center">All</td>
+        </tr>
+        <tr>
+            <td style="text-align: center">17:30</td>
+            <td>Arrival at the hotels</td>
+            <td style="text-align: center"></td>
+        </tr>
+    </tbody>
 </table>
 <p><h5>Note: Detailed schedule can be changed at the last minute.</h5></p>
